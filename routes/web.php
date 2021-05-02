34602d2233df1b016bfafbb174cff045d62334fa
abel  ludeño <abelaalb@gmail.com>
2021-05-02 13:03:45 -0400

    modificacion del archivo docker yml de cofiguracion

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view(view:'welcome');
 });
+
+
+Route::get('/admin', function () {
+    return view(view:'layouts.admin');
+});
+
+
